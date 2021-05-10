15e6d11ea11b25fa203bd2f0c49a4d80ca16f41e
Joao Seidel <joaovseidel@gmail.com>
2021-05-10 09:43:09 -0300

    chore: code base

---
+++ src/test/java/io/joaoseidel/message/MessageApplicationTests.java
@@ -0,0 +1,13 @@
+package io.joaoseidel.message;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
