d4cd23b370b444936568ce662e464e4eb4015013
Joao Seidel <joaovseidel@gmail.com>
2021-05-11 20:35:59 -0300

    feat: add create message model

---
+++ src/main/java/io/joaoseidel/message/adapter/web/model/CreateMessageModel.java
@@ -0,0 +1,25 @@
+package io.joaoseidel.message.adapter.web.model;
+
+import io.joaoseidel.message.domain.MessageType;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+public class CreateMessageModel {
+
+  @Schema(description = "The message subject.", example = "Joao Seidel")
+  private final String target;
+
+  @Schema(description = "The message itself.", example = "Hello, World!")
+  private final String body;
+
+  @Schema(implementation = MessageType.class, description = "The type of the message.")
+  private final String type;
+
+  @Schema(description = "The schedule date of the message.")
+  private final LocalDateTime scheduleDate;
+}
