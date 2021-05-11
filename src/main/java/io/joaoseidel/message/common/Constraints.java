9f900bd6bc7aa7230b7063ba5d47c1009f94cab4
Joao Seidel <joaovseidel@gmail.com>
2021-05-11 20:31:28 -0300

    feat: add constraints class

---
+++ src/main/java/io/joaoseidel/message/common/Constraints.java
@@ -0,0 +1,6 @@
+package io.joaoseidel.message.common;
+
+public class Constraints {
+  public static final String UUID_REGEX =
+      "[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}";
+}
