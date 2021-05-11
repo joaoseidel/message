e4cec7144df96abc9b0522560c01fefd2f616534
Joao Seidel <joaovseidel@gmail.com>
2021-05-11 20:30:31 -0300

    feat: add openApi configuration bean

--- src/main/java/io/joaoseidel/message/MessageApplication.java
+++ src/main/java/io/joaoseidel/message/MessageApplication.java
@@ -1,13 +1,26 @@
 package io.joaoseidel.message;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MessageApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MessageApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(MessageApplication.class, args);
+  }
 
+  public @Bean OpenAPI messageApi() {
+    return new OpenAPI()
+        .info(
+            new Info()
+                .title("Messages API")
+                .description("A simple message scheduler application")
+                .version("v1.0.0")
+                .license(new License().name("MIT").url("https://opensource.org/licenses/MIT")));
+  }
 }
